a21950536e8fd7d390a0b90a6852c6cad5b77261
Kevin S. Tran <tanksense@gmail.com>
2025-04-09 17:27:45 +1000

    Week 6

--- Assets/C4F_TinyTanks/Scripts/Shoot.cs
+++ Assets/C4F_TinyTanks/Scripts/Shoot.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Shoot : MonoBehaviour
 {
     public Projectile proj;
     public Transform muzzle;
+    public UnityEvent onFire;
 
-    public void Fire() { Instantiate(proj, muzzle.position, muzzle.rotation); }
+    public void Fire() { Instantiate(proj, muzzle.position, muzzle.rotation);
+                         onFire.Invoke();
+                       }
 }
